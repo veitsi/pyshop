1e254d748be27c2d258536938c705e5f68f6b3a1
veitsi <xtfkpi@gmail.com>
2017-02-17 10:25:38 +0200

    locally works

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/about/')
